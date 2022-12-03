c3f4629e020ba8b89ea305a6a7a08149a0b1d93d
Donovan Tyler <edatosho@gmail.com>
2022-12-03 16:36:49 -0500

    Update botmain.py
    syntax error

--- botmain.py
+++ botmain.py
@@ -24,17 +24,17 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 @bot.slash_command(name='roll', guild_id=1048352661130461304)
-@option("numDice", description="The number of dice you wish to roll.")
-@option("diceFace", description="The type of die you wish to roll. Acceptable answers are 4, 6, 8, 10, 12, 20, 100")
-async def roll(ctx, numDice: int, diceFace: int):
-    faceTuple = {4, 6, 8, 10, 12, 20, 100}
-    if diceFace not in faceTuple:
+@option("num_dice", description="The number of dice you wish to roll.")
+@option("dice_face", description="The type of die you wish to roll. Acceptable answers are 4, 6, 8, 10, 12, 20, 100")
+async def roll(ctx, num_dice: int, dice_face: int):
+    faceTuple = (4, 6, 8, 10, 12, 20, 100)
+    if dice_face not in faceTuple:
         await ctx.respond("The type of die must be of values: 4, 6, 8, 10, 12, 20, or 100")
 
     totalValue = 0
 
-    for val in itertools.repeat(None, numDice):
-        dieValue = random.randint(1, diceFace)
+    for val in itertools.repeat(None, num_dice):
+        dieValue = random.randint(1, dice_face)
         totalValue = totalValue + dieValue
 
     await ctx.respond(totalValue)
